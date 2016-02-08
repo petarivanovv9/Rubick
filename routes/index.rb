705c4bf996bee9f463a1025457c3859e74a49477
Petar Ivanov <pepincho95@gmail.com>
2016-02-08 23:01:06 +0200

    Show user open groups on index page.

--- routes/index.rb
+++ routes/index.rb
@@ -1,9 +1,16 @@
 class App < Sinatra::Base
   get '/' do
-    if session.has_key?(:username)
+    if session.has_key?(:username) and session.has_key?(:user_id)
       @username = session[:username]
+      @user_open_groups = UserOpenGroup.where(user_id: session[:user_id])
+      @user_open_groups_names = Array.new
+      @user_open_groups.each do |group|
+        group_name = OpenGroup.find(group.open_group_id).name
+        @user_open_groups_names << group_name
+      end
     else
       @username = ""
+      @user_open_groups_names = []
     end
 
     erb :index
