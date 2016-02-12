aa85941740ee89a1b3df20c56a3ad1603e0e9998
Petar Ivanov <pepincho95@gmail.com>
2016-02-12 14:35:04 +0200

    Add some tests about open-group post and comment functionality.

---
+++ spec/features/open_group_post_comment_spec.rb
@@ -0,0 +1,73 @@
+require_relative '../spec_helper.rb'
+
+feature 'Open Groups Post Comment Functionality' do
+  around(:each) do |example|
+    ActiveRecord::Base.connection.transaction do
+
+      User.create(
+        username: 'pesho123',
+        password: '123123')
+
+      visit '/login'
+      fill_in 'username', with: 'pesho123'
+      fill_in 'password', with: '123123'
+
+      click_button 'Login'
+      example.run
+
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  scenario 'Can comment a post if user is admin' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/open_group/rubyyy'
+
+    fill_in 'open_group_post_content', with: 'hiii :)'
+
+    click_button 'Create Post'
+
+    expect(page).to have_content('Create Comment')
+  end
+
+  scenario 'Can\'t comment a post if user is not in group' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/logout'
+
+    visit '/register'
+    fill_in 'username', with: 'batkata'
+    fill_in 'password', with: '123123'
+    fill_in 'password2', with: '123123'
+
+    click_button 'Register'
+
+    visit '/open_group/rubyyy'
+
+    expect(page).to have_no_content('Create Comment')
+  end
+
+  scenario 'Can\'t comment a post if there is no post' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/open_group/rubyyy'
+
+    expect(page).to have_no_content('Create Comment')
+  end
+end
--- spec/features/open_group_spec.rb
+++ spec/features/open_group_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper.rb'
 
-feature 'Open Groups Functionality' do
+feature 'Open Groups Main Functionality' do
   around(:each) do |example|
     ActiveRecord::Base.connection.transaction do
 
@@ -29,41 +29,6 @@ feature 'Open Groups Functionality' do
 
     expect(page).to have_current_path('/open_group/rubyyy') and
                     have_content('Open Group Admin\'s Username: pesho123')
-  end
-
-  scenario 'Can post if user is admin' do
-    visit '/create_open_group'
-
-    fill_in 'open_group_name', with: 'rubyyy'
-    fill_in 'open_group_description', with: 'this is a test group'
-
-    click_button 'Create Open Group'
-
-    visit '/open_group/rubyyy'
-
-    expect(page).to have_content('Create Post')
-  end
-
-  scenario 'Can\'t post if user is not in group' do
-    visit '/create_open_group'
-
-    fill_in 'open_group_name', with: 'rubyyy'
-    fill_in 'open_group_description', with: 'this is a test group'
-
-    click_button 'Create Open Group'
-
-    visit '/logout'
-
-    visit '/register'
-    fill_in 'username', with: 'batkata'
-    fill_in 'password', with: '123123'
-    fill_in 'password2', with: '123123'
-
-    click_button 'Register'
-
-    visit '/open_group/rubyyy'
-
-    expect(page).to have_no_content('Create Post')
   end
 
   scenario 'Can join open group if user is not in group' do
