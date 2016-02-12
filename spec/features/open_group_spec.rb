a01a1c876c74a9a14fd5908b38567d1e22298dec
Petar Ivanov <pepincho95@gmail.com>
2016-02-12 12:04:18 +0200

    Add part-2 of the tests for open groups.

--- spec/features/open_group_spec.rb
+++ spec/features/open_group_spec.rb
@@ -136,12 +136,120 @@ feature 'Open Groups Functionality' do
     expect(page).to have_no_content('Delete rubyyy group')
   end
 
+  scenario 'Shows open group name on index page after creating it' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/'
+
+    expect(page).to have_content('rubyyy')
+  end
+
   scenario 'Shows open group name on index page after joining it' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/logout'
+
+    visit '/register'
+    fill_in 'username', with: 'batkata'
+    fill_in 'password', with: '123123'
+    fill_in 'password2', with: '123123'
+
+    click_button 'Register'
+
+    visit '/open_group/rubyyy'
+
+    click_button 'Join rubyyy group'
+
+    visit '/'
+
+    expect(page).to have_content('rubyyy')
   end
 
   scenario 'Doen\'t show open group name on index page after leaving it' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/logout'
+
+    visit '/register'
+    fill_in 'username', with: 'batkata'
+    fill_in 'password', with: '123123'
+    fill_in 'password2', with: '123123'
+
+    click_button 'Register'
+
+    visit '/open_group/rubyyy'
+
+    click_button 'Join rubyyy group'
+
+    click_button 'Leave rubyyy group'
+
+    visit '/'
+
+    expect(page).to have_no_content('rubyyy')
   end
 
   scenario 'When admin delete open group all members automatically leave it and its name is not on user\'s index pages' do
+    visit '/create_open_group'
+
+    fill_in 'open_group_name', with: 'rubyyy'
+    fill_in 'open_group_description', with: 'this is a test group'
+
+    click_button 'Create Open Group'
+
+    visit '/logout'
+
+    visit '/register'
+    fill_in 'username', with: 'batkata'
+    fill_in 'password', with: '123123'
+    fill_in 'password2', with: '123123'
+
+    click_button 'Register'
+
+    visit '/open_group/rubyyy'
+
+    click_button 'Join rubyyy group'
+
+    visit '/logout'
+
+    visit '/login'
+
+    fill_in 'username', with: 'pesho123'
+    fill_in 'password', with: '123123'
+
+    click_button 'Login'
+
+    visit '/open_group/rubyyy'
+
+    click_button 'Delete rubyyy group'
+
+    visit '/'
+
+    expect(page).to have_no_content('rubyyy')
+
+    visit '/logout'
+
+    visit '/login'
+
+    fill_in 'username', with: 'batkata'
+    fill_in 'password', with: '123123'
+
+    visit '/'
+
+    expect(page).to have_no_content('rubyyy')
   end
 end
